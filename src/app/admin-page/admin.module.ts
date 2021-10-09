2faa69431651ae76a3df35f44d6fe862447e8d57
Vitalich59 <chyrukaevva@gmail.com>
2021-10-09 09:32:09 +0500

    feat(start): Add some component

---
+++ src/app/admin-page/admin.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule} from "@angular/router";
+import {AdminPageComponent} from "./admin-page.component";
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ManageReservationComponent } from './manage-reservation/manage-reservation.component';
+import { ReservationListComponent } from './reservation-list/reservation-list.component';
+
+
+
+@NgModule({
+  declarations: [LoginPageComponent, ManageReservationComponent, ReservationListComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '', component: AdminPageComponent, children: [
+          {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
+          {path: 'login', component: LoginPageComponent},
+          {path: 'manage-reservation', component: ManageReservationComponent},
+          {path: 'reservation-list', component: ReservationListComponent}
+
+        ]
+      }
+    ])
+  ],
+  exports: []
+})
+export class AdminModule { }
---
+++ src/app/admin-page/manage-reservation/manage-reservation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'manage-reservation',
+  templateUrl: './manage-reservation.component.html',
+  styleUrls: ['./manage-reservation.component.css']
+})
+export class ManageReservationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
